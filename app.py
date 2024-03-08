c32f06293468f6c5ebc8879b8f72ca378322d815
R-Nechamkin <118650276+R-Nechamkin@users.noreply.github.com>
2024-03-07 22:12:21 -0500

    Got simple HTML template working

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
 from flask import Flask, render_template
 
+
+class MockDBRow:
+
+    def __init__(self, s, n, c):
+        self.street = s
+        self.name = n
+        self.city = c
+
+
+    def get_street(self):
+        return self.__street
+
+
+    def get_name(self):
+        return self.__name
+
+
+    def get_city(self):
+        return self.__city
+
+
+
+app = Flask(__name__)
+
 @app.route('/')
 def home():
-    rows = [10,5,2]
+    rows = [MockDBRow('Crescent', 'Rivka', 'bang'),
+            MockDBRow('Plastic', 'Devora', 'Passaic')
+            ]
+            
     return render_template('Grid.html', rows = rows)
+
+
