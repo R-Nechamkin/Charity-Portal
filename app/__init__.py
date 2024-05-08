a2ca46a02a285842f624de9540d499624c5b873e
R-Nechamkin <rnechamkin@gmail.com>
2024-05-08 19:29:59 -0400

    Attempted to make it work

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,21 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager 
+from flask_login import LoginManager
 
-# init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy()
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 def create_app():
-    app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite+pysqlite:///db.sqlite'
-
-    db.init_app(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -33,5 +36,7 @@ def create_app():
     # blueprint for non-auth parts of app
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from app import main, auth, models
 
     return app
