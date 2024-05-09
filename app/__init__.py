43e1c3106f4fa3d91ab47ee6b03ea35541622e79
R-Nechamkin <rnechamkin@gmail.com>
2024-05-09 14:49:41 -0400

    got __init__.py working

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,21 @@
-# init.py
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 from .config import Config
 
-app = Flask(__name__)
+
+# init SQLAlchemy so we can use it later in our models
+app = Flask('app')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 
-def create_app():
 
+def create_app():
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -26,8 +25,9 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        return User.query.get(int(user_id))
+        
+        return User.query.get(int(user_id))          
+
 
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
@@ -37,6 +37,9 @@ def create_app():
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from app import main, auth, models
-
     return app
+
+
+create_app()
+
+    
