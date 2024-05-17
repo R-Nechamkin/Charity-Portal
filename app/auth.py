5977d484e054eee0252d53766c9127afcef152c4
R-Nechamkin <rnechamkin@gmail.com>
2024-05-16 22:16:05 -0400

    username need be alphanumeric, fixed error in logic

--- app/auth.py
+++ app/auth.py
@@ -60,7 +60,7 @@ def signup_post():
     
     pattern = r'^[a-zA-Z0-9]*$'
     # We use the username in a lot of random places, so for simplicity's sake, let's require it to be simple
-    if bool(re.match(pattern, name)) and ' ' in name:
+    if not bool(re.match(pattern, name)) or ' ' in name:
         flash('Username must be alphanumeric and cannot contain spaces.')
         return redirect(url_for('auth.signup'))
 
