71977238de4812e04890adacd67e4782d7ad668a
R-Nechamkin <rnechamkin@gmail.com>
2024-05-16 22:03:48 -0400

    added debugging statements

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,5 @@
 # auth.py
+import re
 
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -24,12 +25,19 @@ def login_post():
 
     # check if user actually exists
     # take the user supplied password, hash it, and compare it to the hashed password in database
-    if not user or not check_password_hash(user.password, password): 
+    if not user:
+        print('User does not exist with this email')
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
+
+    if not check_password_hash(user.password, password):
+        print('Invalid password')
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
+    print('Logged in successfully')
     return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
@@ -52,8 +60,9 @@ def signup_post():
     
     pattern = r'^[a-zA-Z0-9]+$'
     # We use the username in a lot of random places, so for simplicity's sake, let's require it to be simple
-    if bool(re.match(pattern, s)) and ' ' not in s:
+    if bool(re.match(pattern, name)) and ' ' not in name:
         flash('Username must be alphanumeric and cannot contain spaces.')
+        return redirect(url_for('auth.signup'))
 
     # #create spreadsheet in database
     # sheet = Spreadsheet.query.filter_by(url=sheet_url).first() # if this returns a user, then the sheet already exists in database
@@ -63,8 +72,8 @@ def signup_post():
         # db.session.commit()
 
     # create new user with the form data. Hash the password so plaintext version isn't saved.
-    new_user = User(email=email, username=name, password=generate_password_hash(password), sheet_id = sheet.sheet_id)
-
+    new_user = User(email=email, username=name, password=generate_password_hash(password))
+    print('created the user')
 
     # add the new user to the database
     db.session.add(new_user)
