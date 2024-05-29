3a1ef997c0ed9cffc26908773e27799a39828f55
R-Nechamkin <rnechamkin@gmail.com>
2024-05-28 22:48:20 -0400

    random improvements

--- app/main.py
+++ app/main.py
@@ -57,20 +57,19 @@ def profile():
     return render_template('profile.html')
 
 
-@main.route('/fields', methods=['GET', 'POST'])
+@main.route('/set-up/num-fields', methods=['GET', 'POST'])
 @login_required
 def fields():
     if request.method == 'POST':
         num_fields = int(request.form.get('num_fields'))
         return redirect('/field_details/' + str(num_fields))
-        
-    
+
     if not current_user.charity.fields:
         flash('Data storage not set up for this organization. Let\'s configure your storage before inserting data.')
     return render_template('fields.html')
 
 
-@main.route('/field_details/<num_fields>', methods=['GET', 'POST'])
+@main.route('/set-up/field-details/<num_fields>', methods=['GET', 'POST'])
 @login_required
 def field_details(num_fields):
     if request.method == 'POST':
@@ -82,7 +81,7 @@ def field_details(num_fields):
             field_details.append((field_name, field_type, field_num))
         create_table(field_details, user=current_user)
         return redirect(url_for('main.index'))
-        
+
     return render_template('field_details.html', num_fields=int(num_fields))
 
 
@@ -91,9 +90,11 @@ def field_details(num_fields):
 def insert_data():
     if not current_user.charity.fields:
         return redirect(url_for('main.fields'))
-        
+
     return render_template('insert-data.html',
-        manual_insertion_link =  url_for('main.manual_insert'), import_data_link = url_for('main.import_data'), upload_data_link = url_for('main.upload_data'))
+                           manual_insertion_link=url_for('main.manual_insert'),
+                           import_data_link=url_for('main.import_data'),
+                           upload_data_link=url_for('main.upload_data'))
 
 
 @login_required
@@ -118,12 +119,13 @@ def see_data():
         row = []
         row.append(record.record_id)
         for field in cols:
-            data = get_datum(record =record, field=field)
+            data = get_datum(record=record, field=field)
             row.append(data)
         content.append(row)
+
     return render_template('see-data.html', headers=headers, rows=content)
-    
-    
+
+
 @login_required
 @main.route('/insert-data/upload', methods=['GET', 'POST'])
 def upload_data():
@@ -134,22 +136,21 @@ def upload_data():
         if not file_ext == '.csv':
             flash('This file is not a CSV file')
             return redirect(url_for('main.upload_data'))
-           
-            
+
         data = pandas.read_csv(file)
         charity = current_user.charity_id
         headers = Field.query.filter_by(charity_id=charity).all()
-        
+
         try:
             insert_user_data(charity=current_user.charity, data=data.values, headers=headers)
-        #TODO: Really I should only catch database exceptions
+        # TODO: Really I should only catch database exceptions
         except Exception as e:
             flash('Something went wrong while importing your data. Check your data and try again.')
             print('Error occurred while importing data', traceback.format_exc(), sep='\n')
             return redirect(url_for('main.upload_data'))
 
         return redirect(url_for('main.see_data'))
-        
+
     return render_template('upload-data.html')
 
 
@@ -158,12 +159,12 @@ def upload_data():
 def import_data():
     if request.method == 'POST':
         form = request.form.to_dict()
-        
+
         try:
             data = get_data_from_api(url=form['url'], sheet_index=(int(form['sheet_num']) - 1))
         except:
             flash('Something went wrong while trying to access your spreadsheet. Check your internet connection,' +
-                'make sure your sheet is actually shared, and that you pasted the right thing, and try again.')
+                  'make sure your sheet is actually shared, and that you pasted the right thing, and try again.')
             print(traceback.format_exc())
             return redirect(url_for('main.import_data'))
 
@@ -174,11 +175,12 @@ def import_data():
                 headers.append(header)
             data = data[1:]
         else:
-            headers = db.session.query(Field.name).filter(Field.charity_id==current_user.charity_id).order_by(Field.order).all()
+            headers = db.session.query(Field.name).filter(Field.charity_id == current_user.charity_id).order_by(
+                Field.order).all()
 
         try:
             insert_user_data(charity=current_user.charity, data=data, headers=headers)
-        #TODO: Really I should only catch database exceptions
+        # TODO: Really I should only catch database exceptions
         except Exception as e:
             flash('Something went wrong while importing your data. Check your data and try again.')
             print(traceback.format_exc())
